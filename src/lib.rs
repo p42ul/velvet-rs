b2bf45c4b61be146ec96fd1a87262d0421f37c46
Paul Buser <buser.paul@gmail.com>
2023-09-09 16:26:33 -0400

    working on convolve

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,28 @@ where
     return (0..len).map(|_| random::<S>()).collect();
 }
 
+pub fn naive_convolve<S>(s1: &Vec<S>, s2: &Vec<S>) -> Vec<S>
+where
+    S: Sample,
+{
+    let (big, small) = if s1.len() > s2.len() {
+        (s1, s2)
+    } else {
+        (s2, s1)
+    };
+    let (len_big, len_small) = (big.len(), small.len());
+    let mut output: Vec<S> = vec![S::EQUILIBRIUM; len_big];
+    for i in 0..len_big {
+        for j in 0..len_small {
+            let big_signal = s1.get(i+j).unwrap_or(&S::EQUILIBRIUM).to_float_sample();
+            let small_signal = s2.get(j).unwrap_or(&S::EQUILIBRIUM).to_float_sample();
+            let product = big_signal.mul_amp(small_signal);
+            output[i] = output[i].add_amp(product.to_signed_sample());
+        }
+    }
+    output
+}
+
 pub fn output_wav<S>(filename: String, buffer: &Vec<S>) -> Result<(), hound::Error>
 where
     S: Sample,
@@ -26,9 +48,8 @@ where
     let mut writer = hound::WavWriter::create(filename, spec)?;
 
     for &sample in buffer.iter() {
-        writer.write_sample(sample.to_sample::<i16>()).unwrap();
+        writer.write_sample(sample.to_sample::<i16>())?;
     }
 
-    writer.finalize()?;
-    Ok(())
+    writer.finalize()
 }
