9e3b08d86c060417e22a4dc2941776436ca98a9d
Paul Buser <buser.paul@gmail.com>
2023-09-30 14:27:38 -0400

    update example

--- examples/convolve_velvet.rs
+++ examples/convolve_velvet.rs
@@ -4,13 +4,10 @@ use dasp::Sample;
 
 fn main() {
     println!("reading triangle.wav");
-    let triangle = velvet_rs::read_wav::<i16>("triangle.wav".to_string())
-        .unwrap()
-        .iter()
-        .map(|&s| s.to_sample::<f32>())
-        .collect();
-    println!("convolving signals");
-    let convolved = velvet_rs::convolve_velvet(&triangle, 44_100 * 2, 2205, 44_100);
+    let triangle = velvet_rs::read_wav::<i16>("triangle.wav".to_string()).unwrap();
+    let sig_length = 44_100 * 5;
+    println!("convolving signal of length {}", sig_length);
+    let convolved = velvet_rs::convolve_velvet(&triangle, sig_length, 2205, 44_100);
     println!("converting from f32 to i16");
     let convolved = convolved
         .iter()
